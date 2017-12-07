d8526b599286cb221b75382d84f757f720f7f072
krzysztofprzybyt <krzysztof.przybyt@gmail.com>
2017-12-07 21:38:59 +0100

    Pierwszy test jednostkowy: dodawanie, odejmowanie.

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        int add = Calculator.add(4, 5);
+        int subtract = Calculator.subtract(7, 4);
+
+        if(add == 9){
+            System.out.println("test dodawania OK");
+        } else {
+            System.out.println("test dodawania - ERROR!");
+        }
+        if(subtract == 3){
+            System.out.println("test odejmowania OK");
+        } else {
+            System.out.println("test odejmowania - ERROR!");
+        }
     }
 }
