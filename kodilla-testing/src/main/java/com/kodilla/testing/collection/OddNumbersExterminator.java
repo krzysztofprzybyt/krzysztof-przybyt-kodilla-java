158f1b930a2a0b7c2083fb78b86f32a4bf697a83
krzysztofprzybyt <krzysztof.przybyt@gmail.com>
2017-12-10 00:43:48 +0100

    Test Collections1

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,4 +1,15 @@
 package com.kodilla.testing.collection;
 
+import java.util.ArrayList;
+
 public class OddNumbersExterminator {
+    public static ArrayList<Integer> exterminate(ArrayList<Integer> numbers){
+        ArrayList<Integer> pair = new ArrayList<Integer>();
+        for(Integer even: numbers){
+            if(even%2 == 0){
+                pair.add(even);
+            }
+        }
+        return pair;
+    }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/collection/CollectionTestSuite.java
@@ -1,4 +1,55 @@
 package com.kodilla.testing.forum.collection;
 
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class CollectionTestSuite {
+    @Before
+    public void before(){
+        System.out.println("Test Collections: begin");
+    }
+    @After
+    public void after(){
+        System.out.println("Test Collections: end");
+    }
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+        //Given
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+
+        //When
+        ArrayList<Integer> pair;
+        pair =  new ArrayList<Integer>(OddNumbersExterminator.exterminate(lista));
+        System.out.println("Test z pustą bazą");
+        //Then
+        Assert.assertEquals(lista, pair);
+
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList(){
+        //Given
+        ArrayList<Integer> lista = new ArrayList<Integer>();
+
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        //When
+        ArrayList<Integer> pairs;
+        pairs =  new ArrayList<Integer>(OddNumbersExterminator.exterminate(lista));
+        System.out.println("Test z danymi");
+        //Then
+        Assert.assertEquals(lista.get(1), pairs.get(0));
+        Assert.assertEquals(lista.get(3), pairs.get(1));
+
+
+
+    }
+
+
 }
