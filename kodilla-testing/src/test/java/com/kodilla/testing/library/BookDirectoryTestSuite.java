3989e75320443b576c506de6fce3ceed6468755c
krzysztofprzybyt <krzysztof.przybyt@gmail.com>
2017-12-14 22:02:12 +0100

    6.5 z mockito

--- kodilla-testing/src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
@@ -9,9 +9,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 public class BookDirectoryTestSuite {
