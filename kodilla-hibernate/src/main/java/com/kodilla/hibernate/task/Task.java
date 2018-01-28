54ba53c473dd720aacfcf93cda11d78205f9b13c
krzysztofprzybyt <krzysztof.przybyt@gmail.com>
2018-01-28 21:49:41 +0100

    13.3 v 0.1

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -1,17 +1,20 @@
 package com.kodilla.hibernate.task;
 
+import com.kodilla.hibernate.tasklist.TaskList;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Entity
 @Table(name="TASKS")
-
 public class Task {
     private int id;
     private String description;
     private Date created;
     private int duration;
+    private TaskFinancialDetails taskFinancialDetails;
+    private TaskList taskList;
 
     public Task(){
 
@@ -22,6 +25,7 @@ public class Task {
         this.created = new Date();
         this.duration = duration;
     }
+
     @Id
     @GeneratedValue
     @NotNull
@@ -29,15 +33,18 @@ public class Task {
     public int getId() {
         return id;
     }
+
     @Column(name="DESCRIPTION")
     public String getDescription() {
         return description;
     }
+
     @NotNull
     @Column(name="CREATED")
     public Date getCreated() {
         return created;
     }
+
     @Column(name="DURATION")
     public int getDuration() {
         return duration;
@@ -57,5 +64,25 @@ public class Task {
 
     private void setDuration(int duration) {
         this.duration = duration;
+    }
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "TASKS_FINANCIALS_ID")
+    public TaskFinancialDetails getTaskFinancialDetails() {
+        return taskFinancialDetails;
+    }
+
+    public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
+        this.taskFinancialDetails = taskFinancialDetails;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "TASKLIST_ID")
+    public TaskList getTaskList() {
+        return taskList;
+    }
+
+    public void setTaskList(TaskList taskList) {
+        this.taskList = taskList;
     }
 }
